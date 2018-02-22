1b17a3413ec8a1947f5cf0fbc34da8e07576f289
Georgi Prodanov <georgiwe@gmail.com>
2018-02-22 21:22:40 +0200

    Add linting for jsx and react. Add nodemon. Refactor webpack config. Move server.js into the server folder.

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,32 @@
 const path = require('path');
 
 const config = {
-  context: `${__dirname}/client`,
+  context: path.resolve('./client'),
   entry: './index.js',
   output: {
-    path: path.resolve(__dirname, 'public'),
-    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public', 'dist'),
+    filename: 'bundle.js'
   },
+
   module: {
-    loaders: [
-      { test: /\.js$/, use: 'babel-loader' }
-    ],
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['react', 'stage-2'] // 'env',
+          }
+        }
+      }
+    ]
   },
+
+  devtool: 'source-map'
 };
 
 // config.plugins.push(new webpack.optimize.UglifyJsPlugin());
 // config.devtool = 'source-map';
 
 module.exports = config;
-
